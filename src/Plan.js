8865eec33e21a9219fe7cbb479a1024060af1c79
Johnny Medhane <134869490+Johnnymedhane@users.noreply.github.com>
2025-01-18 16:39:20 +0200

    Update Plan.js

--- src/Plan.js
+++ src/Plan.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { Button } from "./Button";
 import { AddPersonForm } from "./AddPersonForm";
 import { Participante } from "./Participante";
